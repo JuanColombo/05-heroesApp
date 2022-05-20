96bdea8577534258bf0a9838a4d283e294eed9d0
Juan Colombo <juampicolombosj@gmail.com>
2022-05-20 09:18:48 -0300

    Fin seccion 15 - Rutas y Guards

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interdace';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router : Router, private authService : AuthService) { }
+  
+  get auth(){
+    return this.authService.auth;
+  }
   ngOnInit(): void {
   }
+
+  logout(){
+    this.router.navigate(['./auth'])
+   }
 
 }
